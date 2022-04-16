1f45b3155d5bf51dc6310a140ed4d7f5a83c3ed9
Amber Handal <60497418+amberhandal@users.noreply.github.com>
2022-04-16 15:02:47 -0400

    added register post request

--- frontend/src/Pages/Register/Register.js
+++ frontend/src/Pages/Register/Register.js
@@ -1,35 +1,81 @@
-import React from 'react';
-import './Register.css';
+import React, { useState } from "react";
+import "./Register.css";
 
 export default function Register() {
-  return(
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const newTeacher = {
+      email,
+      firstName,
+      lastName,
+      username,
+      password,
+    };
+    const resp = await fetch("/teachers", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTeacher),
+    });
+  };
+
+  return (
     <div className="login-wrapper">
       <h1>Please Register</h1>
-      <form>
-      <label>
+      <form onSubmit={handleSubmit}>
+        <label>
           <p>Email</p>
-          <input type="text" />
+          <input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            type="text"
+          />
         </label>
         <label>
           <p>First Name</p>
-          <input type="text" />
+          <input
+            value={firstName}
+            onChange={({ target }) => setFirstName(target.value)}
+            type="text"
+          />
         </label>
         <label>
           <p>Last Name</p>
-          <input type="text" />
+          <input
+            value={lastName}
+            onChange={({ target }) => setLastName(target.value)}
+            type="text"
+          />
         </label>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+            type="text"
+          />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            type="password"
+          />
         </label>
         <div>
           <button type="submit">Submit</button>
         </div>
       </form>
     </div>
-  )
+  );
 }
