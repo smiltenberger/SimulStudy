96a7147e42b1b48f27f0606658b23ca6c5acc8eb
Amber Handal <60497418+amberhandal@users.noreply.github.com>
2022-04-19 10:07:41 -0400

    Added content to dashboard

--- frontend/src/Pages/Dashboard/Dashboard.js
+++ frontend/src/Pages/Dashboard/Dashboard.js
@@ -1,8 +1,27 @@
-
-import React from 'react';
+import React from "react";
 
 export default function Dashboard() {
-  return(
-    <h2>Dashboard</h2>
+  return (
+    <>
+      <h2>Dashboard</h2>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Student ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
   );
 }
