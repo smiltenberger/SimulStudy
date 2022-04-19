b35f84348f352d160c9f8b411346b46dd3d8be25
Amber Handal <60497418+amberhandal@users.noreply.github.com>
2022-04-19 10:08:02 -0400

    Can login as student or teacher

--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -15,6 +15,7 @@ async function loginUser(credentials) {
 export default function Login({ setToken }) {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+  const [userType, setUserType] = useState("student");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -22,14 +23,42 @@ export default function Login({ setToken }) {
     const token = await loginUser({
       username,
       password,
+      userType,
+    }).then((response) => {
+      localStorage.setItem("user", userType);
+      const redirectURL = userType === "student" ? "/quizzes" : "/new-quiz";
+      setToken(response.token);
+      navigate(redirectURL);
     });
-    setToken(token);
   };
 
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form onSubmit={handleSubmit}>
+        <p>Student or Teacher</p>
+        <label>
+          <p>Student</p>
+          <input
+            value={userType}
+            name="userType"
+            onChange={({ target }) => setUserType(target.id)}
+            type="radio"
+            checked={userType === "student"}
+            id="student"
+          />
+        </label>
+        <label>
+          <p>Teacher</p>
+          <input
+            value={userType}
+            name="userType"
+            onChange={({ target }) => setUserType(target.id)}
+            type="radio"
+            checked={userType === "teacher"}
+            id="teacher"
+          />
+        </label>
         <label>
           <p>Username</p>
           <input
